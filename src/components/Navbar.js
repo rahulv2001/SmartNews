1e377725072669fdcae2b8179e883dfe1bf4bcfe
Rahul Vishwakarma <rahulsaidupur232103@gmail.com>
2022-07-22 18:58:37 +0530

    third commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" style={{height: '80px'}}>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" style={{ height: '80px' }}>
         <div className="container-fluid">
           <Link className="navbar-brand fw-bold ms-20" to="/">SmartNews</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
